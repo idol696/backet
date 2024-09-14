555408f24aa88efa150b4f157c8c2565fc9f56f0
Roman Kuzmin <ghelatin@mail.ru>
2024-09-14 13:15:33 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/backet/BacketApplication.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.backet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/backet/BacketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.backet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
